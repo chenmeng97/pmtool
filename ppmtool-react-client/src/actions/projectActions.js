17ef84edcaf32a557442314b6a775b09004536f9
chenmeng <chenmengcc1997@gmail.com>
2021-06-06 15:57:07 +0800

    get and display projects

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,10 +1,26 @@
 import axios from "axios";
-import {GET_ERRORS} from "./types";
+import {GET_ERRORS, GET_PROJECTS} from "./types";
 
 export const createProject = (project, history) => async dispatch => {
     try{
         const res = await axios.post("http://localhost:8080/api/project", project);
         history.push("/dashboard");
+    }catch (err){
+        console.log("error: " + err);
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data,
+        })
+    }
+}
+
+export const getProjects = () => async dispatch => {
+    try{
+        const res = await axios.get("http://localhost:8080/api/project/all");
+        dispatch({
+            type: GET_PROJECTS,
+            payload: res.data
+        })
     }catch (err){
         console.log("error: " + err);
         dispatch({
