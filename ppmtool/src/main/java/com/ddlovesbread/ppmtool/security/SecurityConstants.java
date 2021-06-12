8166618e05208c76001e28a4bc5c699c3d548d30
chenmeng <chenmengcc1997@gmail.com>
2021-06-12 17:52:02 +0800

    Generate jwt token for user login

---
+++ ppmtool/src/main/java/com/ddlovesbread/ppmtool/security/SecurityConstants.java
@@ -0,0 +1,11 @@
+package com.ddlovesbread.ppmtool.security;
+
+public class SecurityConstants {
+
+    public static final String SIGNUP_URLS = "/api/users/**";
+    public static final String H2_URL = "h2-console/**";
+    public static final String SECRET = "SecretKeyToGenJWTs";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final long EXPIRATION = 30_000;
+}
--- ppmtool/src/main/java/com/ddlovesbread/ppmtool/web/UserController.java
+++ ppmtool/src/main/java/com/ddlovesbread/ppmtool/web/UserController.java
@@ -1,12 +1,19 @@
 package com.ddlovesbread.ppmtool.web;
 
 import com.ddlovesbread.ppmtool.domain.User;
+import com.ddlovesbread.ppmtool.payload.JWTLoginSuccessResponse;
+import com.ddlovesbread.ppmtool.payload.LoginRequest;
+import com.ddlovesbread.ppmtool.security.JwtTokenProvider;
 import com.ddlovesbread.ppmtool.services.MapValidationErrorService;
 import com.ddlovesbread.ppmtool.services.UserService;
 import com.ddlovesbread.ppmtool.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +21,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.text.Bidi;
+
+import static com.ddlovesbread.ppmtool.security.SecurityConstants.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping("/api/users")
@@ -27,6 +37,25 @@ public class UserController {
 
     @Autowired
     private UserValidator userValidator;
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result){
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap!=null) return errorMap;
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = TOKEN_PREFIX + tokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTLoginSuccessResponse(true, jwt));
+    }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
